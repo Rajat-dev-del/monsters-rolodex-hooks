c7133ef6d1e20ee47eb3a81ffc087e011eff6070
Rajat Srivastava <rajats@clavax.com>
2022-05-02 15:46:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
 
-function App() {
+import { useEffect, useState } from "react";
+
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
+
+const App =()=> {
+  const [searchField , setSearchField] = useState('')
+  const [monsters ,setMonsters]=useState([])
+  const [searchString , setSearchString] = useState('') 
+  const [filterMonsters,setFilteredMoonsters]=useState(monsters) 
+
+ useEffect(()=>{
+   fetch("https://jsonplaceholder.typicode.com/users")
+     .then((Response) => Response.json())
+     .then((users) => {
+       setMonsters(users)
+     })
+ },[])
+  
+
+ useEffect(()=>{
+    const newFilteredMonster = monsters.filter((monster)=>{
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    })
+    setFilteredMoonsters(newFilteredMonster)
+ },[monsters,searchField])
+  
+  
+  const  onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString)
+  }
+  
+  const onChange =(event) =>{
+    setSearchString(event.target.value)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox onChangeHandler = {onSearchChange} placeholder='search monsters' classname='search-box-monster'/>
+      <SearchBox onChangeHandler = {onChange} placeholder='search ' classname='search-box-monster'/>
+        <CardList monsters={filterMonsters} />
     </div>
   );
+  
 }
 
 export default App;
